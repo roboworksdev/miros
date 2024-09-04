f9fe560c82d9304102c2ebcfa9d002ba4097e64b
TheoMangos <130418904+TheoMangos@users.noreply.github.com>
2024-09-05 00:14:24 +1000

    updated

--- miros_install/README.md
+++ miros_install/README.md
@@ -1,6 +1,6 @@
 # Miros install instructions
 unzip miros_install.zip in a directory of your choice (<target-directory>)
 ```
-cd ~/<target-directory>
+cd <target-directory>/miros_install
 sudo bash ./miros_install.sh
 ```
--- miros_install/miros_install.sh
+++ miros_install/miros_install.sh
@@ -1,5 +1,8 @@
 sudo apt-get update
-sudo apt-get install ca-certificates curl tmux git
+sudo apt-get install -y ca-certificates curl tmux git
+sudo service xrdp stop
+sudo apt-get install dbus-x11
+sudo service xrdp start
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -10,5 +13,9 @@ echo \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-sudo apt-get install ./docker-desktop-amd64.deb ./miros_1.0-1.deb
+sudo apt-get install -y ./docker-desktop-amd64.deb ./miros_1.0-1.deb
 sudo gsettings set org.gnome.shell favorite-apps "$(gsettings get org.gnome.shell favorite-apps | sed s/.$//), 'miros.desktop']"
+sudo groupadd docker 
+sudo usermod -aG docker $USER
+#newgrp docker
+echo Please restart your pc to complete the installation
